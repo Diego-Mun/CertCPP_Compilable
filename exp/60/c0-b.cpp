1109a0641c120f84fb99425e8ae736fbe5ffae1d
Diego-Mun <dmunuera@ikerlan.es>
2025-11-17 15:09:39 +0100

    Add files via upload

---
+++ exp/60/c0-b.cpp
@@ -0,0 +1,9 @@
+// EXP60-CPP: Compliant Solution
+// application.cpp
+//#include "library.h"
+/*
+void g() {
+  S s;
+  func(s);
+}
+*/
---
+++ exp/60/nc1.cpp
@@ -0,0 +1,16 @@
+// EXP60-CPP: Noncompliant Code Example
+/*
+struct B {
+  int i, j;
+};
+ 
+struct D : B {
+  float f;
+};
+ 
+extern "Fortran" void func(void *);
+ 
+void foo(D *d) {
+  func(d);
+}
+*/
